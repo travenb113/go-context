4f5797644be4696ec3d1ec246f9e3fed2d2bfa5b
travenb113 <Overwhelmingevil@proton.me>
2025-06-23 23:04:58 +0300

    Add example of Basic HTTP server with context cancellation

---
+++ context-http-cancel/context-http-cancel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	fmt.Println("server: listening on :8090")
+	http.ListenAndServe(":8090", nil)
+}
